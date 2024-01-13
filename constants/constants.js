cad57b1b95dedabcbdcc78718df6c3481dd881b7
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2024-01-13 21:40:20 +0700

    farhan Portfolio

---
+++ constants/constants.js
@@ -0,0 +1,67 @@
+export const NAME = 'Farhan Assegaf';
+
+export const DESIGNATION = `Web Developer`;
+
+export const DETAILS = {
+  Residence: 'Indonesia',
+  City: 'Karawang',
+  Age: '23',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'NextJs Developer',
+    level: '50%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '54%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'assegaffarhan4@gmail.com',
+  PHONE: '+6283823290281',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/fhnasgf28',
+  LINKEDIN: 'https://www.linkedin.com/in/farhanassegaf28/',
+  TWITTER: 'https://twitter.com/iosamajavaid',
+  FACEBOOK: 'https://www.facebook.com/iosamajavaid',
+};
